47a1725fa52844b47050bb31587eed31bd01ce1c
Lester Osana <110283849+LesterOsana18@users.noreply.github.com>
2025-01-14 01:54:05 +0800

    Delete RentEase directory
    Application will be implemented in a different method

--- RentEase/src/RentEase.java
+++
@@ -1,9 +0,0 @@
-//Implementing Class
-import UI.*;
-
-public class RentEase {
-    public static void main(String[] args) {
-        ApplicationFrame app = new ApplicationFrame();
-        app.start();
-    }
-}
