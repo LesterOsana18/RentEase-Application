44dcb15b8ceb9bfb4f68e3e4d5ffdd7fc2502026
Rendel Abainza <abainzarendel11@gmail.com>
2025-02-01 21:55:34 +0800

    Finalize GUI

--- src/controller/MyProfileController.java
+++ src/controller/MyProfileController.java
@@ -1,5 +1,93 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class MyProfileController {
+
+    @FXML
+    private Text balance_due_btn;
+
+    @FXML
+    private Text create_invoice_btn;
+
+    @FXML
+    private Text dashboard_btn;
+
+    @FXML
+    private Button edit_btn;
+
+    @FXML
+    private Button edit_btn1;
+
+    @FXML
+    private Text edit_invoice_btn;
+
+    @FXML
+    private Text help_btn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private Text my_profile_btn;
+
+    @FXML
+    private Text payment_history_btn;
+
+    @FXML
+    private TextField rent_bill_text_field;
+
+    @FXML
+    private TextField rent_bill_text_field2;
+
+    @FXML
+    void balance_due_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void create_invoice_btn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboard_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void edit_btn_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void edit_invoice_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void help_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logout_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void my_profile_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_btn_clicked(MouseEvent event) {
+
+    }
 
 }
