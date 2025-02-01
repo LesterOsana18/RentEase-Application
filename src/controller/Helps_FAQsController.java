85afd26cf6437be55265f87dbdec4aee341c5b52
Rendel Abainza <abainzarendel11@gmail.com>
2025-02-01 21:16:18 +0800

    Update Helps & FAQ Controller

--- src/controller/Helps_FAQsController.java
+++ src/controller/Helps_FAQsController.java
@@ -1,5 +1,75 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class Helps_FAQsController {
+
+    @FXML
+    private Text balance_due_btn;
+
+    @FXML
+    private Text create_invoice_btn;
+
+    @FXML
+    private Text dashboard_btn;
+
+    @FXML
+    private Text edit_invoice_btn;
+
+    @FXML
+    private Button help_btn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private Text my_profile_btn;
+
+    @FXML
+    private Text payment_history_btn;
+
+    @FXML
+    void balance_due_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void create_invoice_btn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboard_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void edit_invoice_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void help_btn_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void my_profile_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_btn_clicked(MouseEvent event) {
+
+    }
 
 }
