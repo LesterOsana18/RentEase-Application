8fc013282e60c95ab75d99ce7618ee7ea3195141
Rendel Abainza <abainzarendel11@gmail.com>
2025-02-01 13:50:10 +0800

    Update Balance Due

--- src/controller/BalanceDueController.java
+++ src/controller/BalanceDueController.java
@@ -1,5 +1,113 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class BalanceDueController {
+
+    @FXML
+    private Text balance_due_btn;
+
+    @FXML
+    private Text create_invoice_btn;
+
+    @FXML
+    private Text dashboard_btn;
+
+    @FXML
+    private Text edit_invoice_btn;
+
+    @FXML
+    private Text help_btn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private Text my_profile_btn;
+
+    @FXML
+    private Text payment_history_btn;
+
+    @FXML
+    private Button payment_history_filter_btn;
+
+    @FXML
+    private ComboBox<?> payment_history_month_cmb_box;
+
+    @FXML
+    private TableView<?> payment_history_table_view;
+
+    @FXML
+    private ComboBox<?> payment_history_year_cmb_box;
+
+    @FXML
+    private Text total_properties_text;
+
+    @FXML
+    private Text total_revenue_text;
+
+    @FXML
+    private Text total_units_text;
+
+    @FXML
+    void balance_due_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void create_invoice_btn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboard_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void edit_invoice_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void help_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logout_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void my_profile_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_filter_btn_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_month_cmb_box_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_year_cmb_box_clicked(ActionEvent event) {
+
+    }
 
 }
