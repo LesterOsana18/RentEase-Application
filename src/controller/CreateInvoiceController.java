d66932c6762345bc3b22360cf2b43ccee5b2d837
Rendel Abainza <abainzarendel11@gmail.com>
2025-02-01 14:26:00 +0800

    update

---
+++ src/controller/CreateInvoiceController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class CreateInvoiceController {
+
+    @FXML
+    private Text balance_due_btn;
+
+    @FXML
+    private Text create_invoice_btn;
+
+    @FXML
+    private Text dashboard_btn;
+
+    @FXML
+    private Text edit_invoice_btn;
+
+    @FXML
+    private Text help_btn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private Text my_profile_btn;
+
+    @FXML
+    private Text payment_history_btn;
+
+    @FXML
+    void balance_due_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void create_invoice_btn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboard_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void edit_invoice_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void help_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logout_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void my_profile_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_btn_clicked(MouseEvent event) {
+
+    }
+
+}
