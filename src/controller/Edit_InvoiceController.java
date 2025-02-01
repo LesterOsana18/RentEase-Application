4906044a4d84bc63511ddcd5559b9c7fd91f56ff
Rendel Abainza <abainzarendel11@gmail.com>
2025-02-01 21:11:54 +0800

    Update Edit Controller

--- src/controller/Edit_InvoiceController.java
+++ src/controller/Edit_InvoiceController.java
@@ -1,5 +1,181 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class Edit_InvoiceController {
+
+    @FXML
+    private Text balance_due_btn;
+
+    @FXML
+    private Text create_invoice_btn;
+
+    @FXML
+    private Text dashboard_btn;
+
+    @FXML
+    private DatePicker date_picker;
+
+    @FXML
+    private Button delete_btn;
+
+    @FXML
+    private Button edit_btn;
+
+    @FXML
+    private Text edit_invoice_btn;
+
+    @FXML
+    private TextField electricity_text_field;
+
+    @FXML
+    private Text help_btn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private CheckBox monthly_advance_chk_box;
+
+    @FXML
+    private TextField monthly_advance_text;
+
+    @FXML
+    private CheckBox monthly_deposit_chk_box;
+
+    @FXML
+    private TextField monthly_deposit_text;
+
+    @FXML
+    private Text my_profile_btn;
+
+    @FXML
+    private TextField note_text;
+
+    @FXML
+    private Text payment_history_btn;
+
+    @FXML
+    private ComboBox<?> property_cmb_box;
+
+    @FXML
+    private TextField rent_bill_text_field;
+
+    @FXML
+    private CheckBox repeat_monthly_chk_box;
+
+    @FXML
+    private CheckBox status_overdue_chk_box;
+
+    @FXML
+    private CheckBox status_paid_chk_box;
+
+    @FXML
+    private CheckBox status_partially_chk_box;
+
+    @FXML
+    private CheckBox status_pending_chk_box;
+
+    @FXML
+    private ComboBox<?> unit_cmb_box;
+
+    @FXML
+    private TextField water_text_field;
+
+    @FXML
+    private TextField wifi_text_field;
+
+    @FXML
+    void balance_due_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void create_invoice_btn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboard_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void delete_btn_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void edit_btn_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void edit_invoice_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void help_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logout_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void monthly_advance_chk_box_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void monthly_deposit_chk_box_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void my_profile_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void payment_history_btn_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void repeat_monthly_chk_box_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void status_overdue_chk_box_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void status_paid_chk_box_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void status_partially_chk_box_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void status_pending_chk_box_clicked(ActionEvent event) {
+
+    }
 
 }
